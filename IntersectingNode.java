1c1d5dde788187837176b4f9d649668f392c1923
unknown <divyaraoprep@gmail.com>
2025-02-17 11:57:39 -0500

    Done Linked List 2

---
+++ IntersectingNode.java
@@ -0,0 +1,44 @@
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Use the difference in lengths of the list to find the position to start the pointers. 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = 0;
+        int lengthB = 0;
+        ListNode curr = headA;
+        while (curr != null) {
+            curr = curr.next;
+            lengthA++;
+        }
+        curr = headB;
+        while (curr != null) {
+            curr = curr.next;
+            lengthB++;
+        }
+        while (lengthA > lengthB) {
+            headA = headA.next;
+            lengthA--;
+        }
+        while (lengthB > lengthA) {
+            headB = headB.next;
+            lengthB--;
+        }
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+}
---
+++ ReOrderList.java
@@ -0,0 +1,49 @@
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+//Find the middle of list, reverse the second half and then merge.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while (fast != null) {
+            ListNode temp = slow.next;
+            slow.next = fast;
+            fast = fast.next;
+            slow.next.next = temp;
+            slow = temp;
+        }
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode prev = null;
+        ListNode curr = node;
+        ListNode temp;
+        while (curr != null) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+}
